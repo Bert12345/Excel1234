35590e3fe5d3555e731c2dfc414487726d4040ea
Bert12345 <38731497+Bert12345@users.noreply.github.com>
2018-12-14 09:46:03 +0100

    update

--- Excel/MainWindow.xaml.cs
+++ Excel/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -40,5 +41,70 @@ namespace Excel
             lvItems.Items.Add(sp);
            
         }
-    }
+
+        public void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Set filter for file extension and default file extension
+            dlg.DefaultExt = ".qcv";
+            dlg.Filter = "Text documents (.qcv)|*.qcv";
+
+            // Display OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                
+              
+            }
+
+        }
+
+        private void BtnGo_Click(object sender, RoutedEventArgs e, string filename)
+        {
+            var data = new List<Items>();
+            var lineParser = new ItemsConvert();
+            foreach (string line in File.ReadAllLines(@"" + filename + ""))
+            {
+                if (line.StartsWith("FlatNet")) continue;
+                var objTmp = lineParser.ParseLine(line);
+                if (objTmp != null) data.Add(objTmp);
+            }
+        }
+
+        class Items
+        {
+            public string Signal { get; set; }
+            public int Testpoints { get; set; }
+            public string Connections { get; set; }
+   
+          
+        }
+
+        class ItemsConvert
+        {
+            public Items ParseLine(string line)
+            {
+                Items result = null;
+                var tmp = line.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                if (tmp.Length == 7)
+                {
+                    result = new Items
+                    {
+                        Signal = tmp[0],
+                        Testpoints = Convert.ToInt32(tmp[1]),
+                        Connections = tmp[2],                      
+                    };
+                }
+                return result;
+            }
+        }
+
+   
+}
 }
